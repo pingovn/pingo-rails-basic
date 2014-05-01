7015a9fde2add52f1904d0d865867537fe344f91
Quy Doan <quydoantran@gmail.com>
2014-05-02 00:44:05 +0700

    Block and Iterator

---
+++ 5_block_2.rb
@@ -0,0 +1,6 @@
+def who_says_what
+  yield("Dave", "hello")
+  yield("Andy", "goodbye")
+end
+
+who_says_what {|person, phrase| puts "#{person} says #{phrase}"}
---
+++ 6_iterators.rb
@@ -0,0 +1,8 @@
+#!/usr/bin/env ruby
+
+animals = %w( ant bee cat dog elk ) # create an array
+animals.each {|animal| puts animal } # iterate over the contents
+
+
+5.times { print "*" }
+3.upto(6) {|i| print i }
