46528fe3ebca12f1e34a06cb1e59db986e5a8388
Quy Doan <quydoantran@gmail.com>
2014-05-02 01:17:36 +0700

    Advanced Action Pack

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Blog::Application.routes.draw do
   root :to => "articles#index"
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
+  resources :users
+  resource :session
+  match '/login' => "sessions#new", :as => "login"
+  match '/logout' => "sessions#destroy", :as => "logout"  
 end
