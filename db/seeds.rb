3334725f38232e36165c8efb4b0b55b1d20d14d1
Quy Doan <quydoantran@gmail.com>
2014-05-02 01:16:28 +0700

    Action Pack: Working with the View and the Controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ :name => 'Chicago' }, { :name => 'Copenhagen' }])
-#   Mayor.create(:name => 'Daley', :city => cities.first)
+user = User.create :email => 'mary@example.com', 
+                   :password => 'guessit',
+                   :password_confirmation => 'guessit'
+Category.create [{:name => 'Programming'},
+                 {:name => 'Event'},
+                 {:name => 'Travel'},
+                 {:name => 'Music'},
+                 {:name => 'TV'}]
+user.articles.create :title => 'Advanced Active Record',
+             :body => "Models need to relate to each other. In the real world, ..",
+             :published_at => Date.today
+user.articles.create :title => 'One-to-many associations',
+             :body => "One-to-many associations describe a pattern ..",
+             :published_at => Date.today
+user.articles.create :title => 'Associations',
+             :body => "Active Record makes working with associations easy..",
+             :published_at => Date.today
